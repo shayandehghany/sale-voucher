2b01cdc4d5504ce6162fb43e559823d0234b916a
shayandehghany <shayandt138p@gmail.com>
2025-08-09 19:07:08 +0330

    project structure

---
+++ __manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'sale voucher',
+    'version': '1.0',
+    'summary': 'create a sale voucher factor',
+    'sequence': 0,
+    'description': '''sale voucher management''',
+    'category': 'Productivity',
+    'website': 'https://www.odoo.com/page/billing',
+    'license': 'LGPL-3',
+    'depends': ['base',
+                'mail',
+                'contacts'
+                ],
+    'data': [
+
+    ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+
+}
